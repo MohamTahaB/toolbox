343efad90c27c05c6c63101f2e8223a4b53df4ee
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-01-29 21:30:14 +0100

    initiate storage.json

--- todo/storage/storage.go
+++ todo/storage/storage.go
@@ -10,6 +10,7 @@ import (
 // InitiateStorage checks for the JSON file, creates it if necessary.
 // Returns the JSON directory to be manipulated later by transactors, and an error if there are issues.
 func InitiateStorage() (string, error) {
+
 	usr, err := user.Current()
 	if err != nil {
 		return "", fmt.Errorf("error retrieving the current user: %v", err)
@@ -17,14 +18,19 @@ func InitiateStorage() (string, error) {
 
 	fileName := filepath.Join(usr.HomeDir, ".toolbox", ".todo", "storage.json")
 
-	// Create the dir if does not exist.
+	// Check if the JSON file already exists.
+	if _, err := os.Stat(fileName); os.IsExist(err) {
+		return fileName, nil
+	}
+
+	// Create the dir when it does not exist.
 	if err = os.MkdirAll(filepath.Dir(fileName), 0755); err != nil {
 		return "", fmt.Errorf("error creating directory: %v", err)
 	}
 
-	// Create or open the hidden JSON file.
-	file, err := os.Create(fileName)
-	if err != nil {
+	file, createErr := os.Create(fileName)
+	// Checks if there was an error when creating the JSON file.
+	if createErr != nil {
 		return "", fmt.Errorf("error creating file: %v", err)
 	}
 	defer file.Close()
