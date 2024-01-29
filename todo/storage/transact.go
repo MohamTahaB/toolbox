e517bea7fb19840b5ac8e8f0f03d1e12ad4a0f74
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-01-29 23:00:58 +0100

    create add task

--- todo/storage/transact.go
+++ todo/storage/transact.go
@@ -2,6 +2,17 @@ package storage
 
 import "github.com/toolbox/todo/model"
 
-func (sf *StorageFile) AddTask(t *model.Task) {
+func (sf *StorageFile) AddTask(t *model.Task) error {
 
+	// Retrieve the model from the JSON file.
+	m, err := RetrieveModel()
+	if err != nil {
+		return err
+	}
+
+	// Append the new task to the task list.
+	m.TasksList = append(m.TasksList, *t)
+
+	// Commit changes.
+	return CommitModel(m)
 }
