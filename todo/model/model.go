a8bc8db2642dca0f042888e2d2f3ade19e85319b
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-01-29 23:03:33 +0100

    add json tags to the model struct

--- todo/model/model.go
+++ todo/model/model.go
@@ -2,8 +2,8 @@ package model
 
 // Model structure, consists of a list of all tasks, and the index of the selected one.
 type Model struct {
-	TasksList []Task
-	Selected  int
+	TasksList []Task `json:"taskList"`
+	Selected  int    `json:"selected"`
 }
 
 // Task structure, consists of a title and a boolean that describes its state.
