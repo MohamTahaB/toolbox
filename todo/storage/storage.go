df3acff5aa2fc51c97b8d8406d3c4fbdac7487ee
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-01-29 21:51:10 +0100

    add retrieve model

--- todo/storage/storage.go
+++ todo/storage/storage.go
@@ -1,16 +1,18 @@
 package storage
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/user"
 	"path/filepath"
+
+	"github.com/toolbox/todo/model"
 )
 
 // InitiateStorage checks for the JSON file, creates it if necessary.
 // Returns the JSON directory to be manipulated later by transactors, and an error if there are issues.
 func InitiateStorage() (string, error) {
-
 	usr, err := user.Current()
 	if err != nil {
 		return "", fmt.Errorf("error retrieving the current user: %v", err)
@@ -35,4 +37,30 @@ func InitiateStorage() (string, error) {
 	}
 	defer file.Close()
 	return fileName, nil
+}
+
+// RetrieveModel serves as a call to get the content of the JSON file, that each transaction will do, similarly to a SQL migrate.
+func RetrieveModel() (*model.Model, error) {
+	fileName, err := InitiateStorage()
+
+	// Check if there was an error initiating the storage.
+	if err != nil {
+		return nil, err
+	}
+
+	// Read the file.
+	b, err := os.ReadFile(fileName)
+	// Check if reading the file went error free.
+	if err != nil {
+		return nil, fmt.Errorf("error reading the file: %v", err)
+	}
+
+	// Initiate a ptr to the model.
+	var m *model.Model
+	// Unmarshal the byte slice.
+	if err = json.Unmarshal(b, m); err != nil {
+		return nil, fmt.Errorf("error unmarshalling the file: %v", err)
+	}
+
+	return m, nil
 }
