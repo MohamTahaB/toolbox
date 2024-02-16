bebbc682e877c0f29282cbb9c5c3e03eee7b63fc
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-02-16 13:52:11 +0100

    add file info package

---
+++ jotter/storage/fileInfo/fileInfo.go
@@ -0,0 +1,20 @@
+package fileinfo
+
+type FileInfo struct {
+	Title   string
+	Content string
+}
+
+func (fi *FileInfo) GetTitle() string {
+	if fi == nil {
+		return ""
+	}
+	return fi.Title
+}
+
+func (fi *FileInfo) GetContent() string {
+	if fi == nil {
+		return ""
+	}
+	return fi.Content
+}
