2d5909977de1a95f8e23a0184c306b59b9c0ff07
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-02-16 13:19:41 +0100

    Feature/initiate-model
    initiate model and add constant key mapping and help section

--- jotter/model/model.go
+++ jotter/model/model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"toolbox/jotter/storage"
+
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/viewport"
@@ -16,8 +18,39 @@ const (
 )
 
 type Model struct {
-	State state
-	List  list.Model
+	State    state
+	List     list.Model
 	FileEdit textarea.Model
 	ViewPort viewport.Model
+}
+
+// Initiates the app model.
+func InitiateModel() (*Model, error) {
+	var m Model
+
+	// Initiate the storage.
+	JSONDir, _, err := storage.InitiateStorage()
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the JSON file.
+	fileMap, err := storage.Unmarshal(JSONDir)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build list items slice.
+	var filesSlice []list.Item
+	for _, file := range *fileMap {
+		filesSlice = append(filesSlice, file)
+	}
+
+	// Initiate the list and the file edit.
+	m.List = list.New(filesSlice, list.NewDefaultDelegate(), 0, 0)
+
+	m.List.Title = "Jotter"
+
+	return &m, nil
+
 }
