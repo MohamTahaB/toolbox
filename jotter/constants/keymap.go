06f76ca0e406d734615cde696f187b93f3f9cecf
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-02-27 10:31:56 +0100

    init the keymaps package

---
+++ jotter/constants/keymap.go
@@ -0,0 +1,34 @@
+package constants
+
+import "github.com/charmbracelet/bubbles/key"
+
+type KeyMap struct {
+	Up     key.Binding
+	Down   key.Binding
+	Quit   key.Binding
+	Enter  key.Binding
+	Create key.Binding
+}
+
+var HelpKeyMap = KeyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up"),
+		key.WithHelp("↑", "move up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down"),
+		key.WithHelp("↓", "move down"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esc", "ctrl+c"),
+		key.WithHelp("q", "quit"),
+	),
+	Enter: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("↪", "Enter"),
+	),
+	Create: key.NewBinding(
+		key.WithKeys("n", "w"),
+		key.WithHelp("n | w", "Create new ..."),
+	),
+}
