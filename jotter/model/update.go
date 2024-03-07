caab04b02afbc5efb1c7ec5e4630e53b9203a363
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-03-08 00:05:36 +0100

    linter fix

--- jotter/model/update.go
+++ jotter/model/update.go
@@ -37,7 +37,6 @@ func (m Model) Update(msg tea.Msg) (tea.Msg, tea.Cmd) {
 	return m, cmd
 }
 
-
 // Handles the key messages, according to the state of the model.
 func handleKeyMsg(m *Model, msg *tea.KeyMsg, cmd *tea.Cmd) {
 	switch m.State {
@@ -59,7 +58,7 @@ func handleKeyMsg(m *Model, msg *tea.KeyMsg, cmd *tea.Cmd) {
 			var err error
 			if m.CurrentFile.Content, err = storage.Pull(m.CurrentFile.ID); err != nil {
 				m.CurrentFile.Content = "error reading the file"
-			} 
+			}
 
 		// Create a new list item.
 		case key.Matches(*msg, constants.HelpKeyMap.Create):
@@ -107,7 +106,7 @@ func handleKeyMsg(m *Model, msg *tea.KeyMsg, cmd *tea.Cmd) {
 		case key.Matches(*msg, constants.HelpKeyMap.Enter):
 			(*m).State = ReadFileList
 			(*m).List.InsertItem(0, filelist.FileItem{
-				ID: m.CurrentFile.ID,
+				ID:        m.CurrentFile.ID,
 				FileTitle: m.CurrentFile.Title,
 				// TODO! add the description to the current file.
 			})
@@ -123,7 +122,9 @@ func handleKeyMsg(m *Model, msg *tea.KeyMsg, cmd *tea.Cmd) {
 
 			//Check if the state of the form is complete:
 			if (*m).Form.State == huh.StateCompleted {
-				storage.Push(m.CurrentFile.ID, m.CurrentFile.Content)
+				if err := storage.Push(m.CurrentFile.ID, m.CurrentFile.Content); err != nil {
+					m.CurrentFile.Content = "error writing the file"
+				}
 				(*m).State = ReadFileList
 			}
 
