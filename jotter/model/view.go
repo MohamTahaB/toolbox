ebe6980d15497ad3bcc8182f1f47b9faf3672eff
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-03-14 08:56:41 +0100

    Fix/initiate-view: initiate the model view.

--- jotter/model/view.go
+++ jotter/model/view.go
@@ -1,5 +1,28 @@
 package model
 
+import (
+	"fmt"
+	"strings"
+)
+
 func (m Model) View() string {
+	var b strings.Builder
+	var msg string
+
+	// Render the appropriate section according to the state of the model.
+	switch m.State {
+	case ReadFileList:
+		// Render the list of files.
+		msg = m.List.View()
+	case ReadFile:
+		// Render the file viewport.
+		msg = m.ViewPort.View()
+	case WriteFile:
+		// Render the form.
+		msg = m.Form.View()
+	}
+
+	fmt.Fprintf(&b, "%s \n", msg)
+
 	return ""
 }
